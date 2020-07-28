08e887238e4e9fa0ef9d66f726a2c84b4d6d02cc
Amitabh Ghosh <Sabrthor@users.noreply.github.com>
2020-07-28 22:11:15 +0530

    Update list_resourcegroups.sh

--- lab_1/list_resourcegroups.sh
+++ lab_1/list_resourcegroups.sh
@@ -3,3 +3,15 @@ az account show
 
 # list all resource groups
 az group list -o table
+
+# list all vms
+az vm list -o table
+
+# update azure advisor recommendations
+az advisor configuration show
+
+# list azure advisor recommendations
+az advisor recommendation list --category cost
+az advisor recommendation list --category performance
+az advisor recommendation list --category HighAvailability
+az advisor recommendation list --category Security
